b406fe249270aaf207cd245c3f864abdd57c96ca
Andrey Beyresh <a_beyresh@wargaming.net>
2021-11-18 18:32:17 +0300

    Start make shader loader

--- src/Utils/ShaderLoader.cpp
+++ src/Utils/ShaderLoader.cpp
@@ -1,11 +1,32 @@
 #include "ShaderLoader.h"
 
-#include "MainWindow.h"
+#include <assert.h>
 
+
+#include "MainWindow.h"
 #include "Files.h"
-#include "rapidjson/rapidjson.h"
+#include "rapidjson/document.h"
+
+using namespace rapidjson;
 
 void ShaderLoader::ReadShaders(const std::string &pathToJson)
 {
+    Document document;
+    document.Parse(Files::ReadFile(pathToJson).c_str());
+    assert(document.IsObject());
+    auto doc = document.GetObject();
+    assert(doc.HasMember("Shaders"));
+    assert(doc.HasMember("ShaderPrograms"));
+
+    auto shaders = doc["Shaders"].GetArray();
+    auto shaderPrograms = doc["ShaderPrograms"].GetArray();
+
+    ShaderStorage& storage = MainWindow::Get()->GetScene()->GetShaderStorage();
+
+    for(auto& i: shaders)
+    {
+
+    }
+
 
 }
